cf2a4033b3d2c7876b7d6adbe18a980a9bc2d693
Teanna <teannamtaylor95@gmail.com>
2023-07-22 09:49:52 -0700

    React setup

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { useState, useEffect } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+  return (
+    <div className="App">
+       <h1>Page Count: {count}</h1>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
